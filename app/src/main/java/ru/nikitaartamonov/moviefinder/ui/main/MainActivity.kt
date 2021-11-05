f8e663b6fe2b5df9275021bd8330b8d862de6633
Nikita Artamonov <nikitaartamonov@mail.ru>
2021-11-05 15:09:19 +0300

    lesson2: created activity_main layout (bottom navigation view + menu, custom toolbar)

--- app/src/main/java/ru/nikitaartamonov/moviefinder/ui/main/MainActivity.kt
+++ app/src/main/java/ru/nikitaartamonov/moviefinder/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.nikitaartamonov.moviefinder
+package ru.nikitaartamonov.moviefinder.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.nikitaartamonov.moviefinder.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
