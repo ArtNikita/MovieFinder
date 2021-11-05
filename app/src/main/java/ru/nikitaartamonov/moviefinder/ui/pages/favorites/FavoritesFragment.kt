cc7cbc8e97bb34ba1a08908b90f15eec7f384b8c
Nikita Artamonov <nikitaartamonov@mail.ru>
2021-11-05 19:21:38 +0300

    lesson2: added viewBinding delegate for fragments and initialized bindings in fragments. Also Ctrl+Alt+L some files

--- app/src/main/java/ru/nikitaartamonov/moviefinder/ui/pages/favorites/FavoritesFragment.kt
+++ app/src/main/java/ru/nikitaartamonov/moviefinder/ui/pages/favorites/FavoritesFragment.kt
@@ -1,7 +1,10 @@
 package ru.nikitaartamonov.moviefinder.ui.pages.favorites
 
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import ru.nikitaartamonov.moviefinder.R
+import ru.nikitaartamonov.moviefinder.databinding.FragmentFavoritesBinding
 
-class FavoritesFragment: Fragment(R.layout.fragment_favorites) {
+class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
+    private val binding: FragmentFavoritesBinding by viewBinding(FragmentFavoritesBinding::bind)
 }
